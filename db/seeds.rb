b0d14fdbb6e1ac69e558c8744992aa02b958800c
MaryT573 <81441711+MaryT573@users.noreply.github.com>
2022-07-30 21:35:33 -0600

    user story 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+CustomerItem.destroy_all
+Customer.destroy_all
+Item.destroy_all
+Supermarket.destroy_all
+
+super1 = Supermarket.create(name: 'Publix', location: 'Beaufort')
+super2 = Supermarket.create(name: 'Safeway', location: 'Denver')
+
+customer1 = super1.customers.create(name: "John")
+customer2 = super1.customers.create(name: "Sarah")
+customer3 = super2.customers.create(name: "Eliza")
+customer4 = super2.customers.create(name: "George")
+
+item1 = Item.create(name: "toothpaste", price: 3)
+item2 = Item.create(name: "apple", price: 1)
+item3 = Item.create(name: "chicken", price: 6)
+item4 = Item.create(name: "spinach", price: 2)
+
+custitem1 = CustomerItem.create(customer: customer1, item: item1)
+custitem1 = CustomerItem.create(customer: customer1, item: item2)
+custitem1 = CustomerItem.create(customer: customer2, item: item3)
+custitem1 = CustomerItem.create(customer: customer2, item: item4)
+custitem1 = CustomerItem.create(customer: customer3, item: item1)
+custitem1 = CustomerItem.create(customer: customer3, item: item2)
+custitem1 = CustomerItem.create(customer: customer4, item: item3)
---
+++ spec/features/customers/show_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe "customer show" do
+  describe 'features' do
+    it "can display a list of items associated with the customer and the name of the supermarket" do
+      super1 = Supermarket.create(name: 'Publix', location: 'Beaufort')
+
+      customer1 = super1.customers.create(name: "John")
+      customer2 = super1.customers.create(name: "Sarah")
+
+      item1 = Item.create(name: "toothpaste", price: 3)
+      item2 = Item.create(name: "apple", price: 1)
+      item3 = Item.create(name: "chicken", price: 6)
+      item4 = Item.create(name: "spinach", price: 2)
+
+      custitem1 = CustomerItem.create(customer: customer1, item: item1)
+      custitem1 = CustomerItem.create(customer: customer1, item: item2)
+      custitem1 = CustomerItem.create(customer: customer1, item: item3)
+      custitem1 = CustomerItem.create(customer: customer2, item: item4)
+
+      visit "/customers/#{customer1.id}"
+      expect(current_path).to eq("/customers/#{customer1.id}")
+
+      expect(page).to_not have_content(customer2.name)
+
+      expect(page).to have_content(customer1.name)
+      expect(page).to have_content(super1.name)
+      
+      within "#customer-item" do
+        expect(page).to have_content(item1.name)
+        expect(page).to have_content(item2.name)
+        expect(page).to have_content(item3.name)
+        expect(page).to_not have_content(item4.name)
+      end
+    end
+
+    it 'can display total price of items' do
+
+    end
+  end
+end
